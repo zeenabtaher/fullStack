6f31a902d4b839fe77575f2680a15ddbba9476cb
zeenabtaher <zeenab.taher@gmail.com>
2022-04-02 21:22:09 +0300

    Tehtävät 1.6.-1.10.

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,39 +1,54 @@
 import { useState } from 'react'
 
+const Statistics = ({good, bad, neutral}) => {
+const all = good + neutral + bad
+const average = (good + neutral + bad) / 3
+const positive = good / (neutral + bad)*100
+
+if (all == 0){
+  return (
+    <div>
+      No feefback given
+    </div>
+  )
+}
+  return(
+    <div>
+      <StatisticLine text = "Good" value = {good}/>
+      <StatisticLine text = "Neutral" value = {neutral}/>
+      <StatisticLine text = "Right" value = {bad}/>
+      <StatisticLine text = "All" value = {all}/>
+      <StatisticLine text = "Average" value = {average}/>
+      <StatisticLine text = "Positive" value = {positive}/>
+    </div>
+  )
+}
+
+const StatisticLine = (props) => (
+  <p>{props.text} {props.value}</p>
+)
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   const [left, setGood] = useState(0) //tilojen(hookien) left ja righ alustus alkuarvolla 0:lla
   const [midle, setNeutral] = useState(0)
   const [right, setBad] = useState(0)
-  //const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () =>{
-    //setAll(allClicks.concat('Good'))
-    setGood(left + 1)
-  }
-
-  const handleRightClick = () => {
-    //setAll(allClicks.concat('Bad'))
-    setBad( right +1)
-  } 
-
-  const handleMidleClick =() => {
-    setNeutral(midle + 1)
-  }
-
+  
   return (
     <div>
       <h1>Give feedback</h1>
       <div>
-        <button onClick={handleLeftClick}>good</button>
-        <button onClick={handleMidleClick}>neutral</button>
-        <button onClick={handleRightClick}>bad</button>
+        <Button handleClick = {() => setGood(left + 1)} text = "good" />
+        <Button handleClick={() => setNeutral(midle + 1)} text = "neutral" />
+        <Button handleClick= {() => setBad(right + 1)} text = "bad" />
         <h2>Statistics</h2>
-        <p>Good {left}</p>
-        <p>Neutral {midle}</p>
-        <p>Bad {right}</p>
-        <p>all {left + midle + right}</p>
-        <p>average {(left + midle + right)/3}</p>
-        <p>Positive {left / (midle+ right)*100} %</p>
+        <Statistics good = {left} neutral = {midle} bad= {right}/>
       </div> 
     </div>
   )
