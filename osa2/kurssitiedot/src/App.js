5b64e913272613381e28f1d0c543b41b56265961
zeenabtaher <zeenab.taher@gmail.com>
2022-04-12 21:57:33 +0300

    tehtävä 2.4

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,5 +1,5 @@
 const App = () => {
-  const course = {
+  /*const course = {
     name: 'Half Stack application development',
     id: 1,
     parts: [
@@ -19,46 +19,94 @@ const App = () => {
         id: 3
       }
     ]
-  }
+  }*/
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
  
   return (
     <div>
-        {/*<ul>{course.parts.map(parts => <li>{parts.name}</li>)}</ul>*/}
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      <Courses courses={courses} />
     </div>
   )
 }
 
-const Course = ({course}) => {
-  
+const Courses = ({courses}) => {
   return (
     <div>
-      <Header course={course.name}/>
-      <ul>
-       {course.parts.map(parts => 
-          <Content key={parts.id} osat = {parts}/>
-        )}
-        <Total osat={course.parts} />
-        </ul>
-         
+      {courses.map(({name, id, parts}) => (
+       <Content key={id} osat={parts} name={name}/> 
+      ))}
     </div>
   )
 }
 
 
-const Header = (props) =>{  
-  return(
-    <h1> {props.course} </h1>
-  )
-}
-const Content = ({osat}) => {
+const Header = ({name}) => <h2> {name} </h2>
+
+const Content = ({osat, name}) => {
 return(
   <div>
-    <p>{osat.name} {osat.exercises}</p>
-   
+    <Header name={name}/>
+    <Osat osat={osat}/>  
+    <Total osat={osat}/>
   </div>
 )
 } 
+
+const Osat = ({osat}) => {
+  return(
+    <div>
+      {osat.map(({name, id, exercises}) => (
+         <p key={id}>
+        {name} {exercises} 
+        </p>
+      ))}
+    </div>
+  )
+}
 
 const Total = ({osat}) => {
   const yhteensa = osat.reduce((eka, loput) => 
