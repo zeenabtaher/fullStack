0541965c0ad3a9e314b57beed7b4cc95c905cd54
zeenabtaher <zeenab.taher@gmail.com>
2022-05-31 10:29:56 +0300

    3.10 - step10 - backend internettiin

--- osa3/index.js
+++ osa3/index.js
@@ -107,7 +107,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
