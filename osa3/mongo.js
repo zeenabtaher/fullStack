59a8c8b5f15d26839a825a89c08831036de023c3
zeenabtaher <zeenab.taher@gmail.com>
2022-06-02 16:51:43 +0300

    3.12 - tietokanta komentoriviltä

---
+++ osa3/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+
+
+//luodaan tietokantaan uusi dokumentti komennolla node mongo.js salasana, jossa salasana on crusteria luotaessa annettu salasana
+const password = process.argv[2]
+const nimi = process.argv[3]
+const numero = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.xn2nxzf.mongodb.net/personApp?retryWrites=true&w=majority`
+mongoose.connect(url)
+
+//Määritellään muistiinpanon skeema
+const personSchema = new mongoose.Schema({
+  nimi: String,
+  numero: String,
+})
+//skeemaa vastaava model, joka luo kopion skeemasta
+const Person = mongoose.model('Person', personSchema)
+
+//luodaan muistiinpanoa vastaava olio modelin avulla
+const person = new Person({
+  nimi: nimi,
+  numero: numero,
+})
+if (process.argv.length === 3) {
+    console.log('puhelinluettelo:')
+    Person.find({}).then((result) => {
+        result.forEach((person) => {
+            console.log(`${person.nimi} ${person.numero}`)
+        })
+        mongoose.connection.close()
+        process.exit(1)
+    })
+  }
+
+person.save().then(result => {
+  console.log('Tallennettiin ' + `${nimi}  ${numero}` + ' puhelinluetteloon')
+  mongoose.connection.close() //tietokantayhteys suljetaan
+})
+
+
