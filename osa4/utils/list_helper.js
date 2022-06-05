1dfdba14edd3aa1ff6e3258ab9191aaa5403ac07
zeenabtaher <zeenab.taher@gmail.com>
2022-06-05 17:55:11 +0300

    4.5 - step3

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -12,8 +12,24 @@ else
     }
     return yhteensa
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length == 0)
+        return 0
+    else
+        favorite = blogs[0]
+        likes = 0
+        for (let i=0; i < blogs.length; i++) {
+            if (likes < blogs[i].likes) {
+                favorite= blogs[i]
+                likes = blogs[i].likes
+            }
+        }
+        return favorite
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   } 
