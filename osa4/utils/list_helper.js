c1921ca59f06f1bb26721bfd1f0b3046e497c181
zeenabtaher <zeenab.taher@gmail.com>
2022-06-05 13:34:17 +0300

    4.4 - step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+if (blogs.length == 0)
+    return 0
+else
+    yhteensa = 0
+    for (let i=0; i < blogs.length; i++) {
+        yhteensa += blogs[i].likes
+    }
+    return yhteensa
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   } 
