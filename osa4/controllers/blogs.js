74a64b07f3ca15fbff66665462014bf1be2a60dd
zeenabtaher <zeenab.taher@gmail.com>
2022-06-06 21:11:15 +0300

    4.8 - step1

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,15 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
+/*blogsRouter.get('/', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
+})*/
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs.map((blog) => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog.toJSON())
+})
+/*blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
 
   blog
@@ -17,6 +26,6 @@ blogsRouter.post('/', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
-})
+})*/
 
 module.exports = blogsRouter
---
+++ osa4/tests/blog_api.test.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const testiBlogi = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(testiBlogi[0])
+  await blogObject.save()
+  blogObject = new Blog(testiBlogi[1])
+  await blogObject.save()
+})
+
+describe('blogilista JSON-muodoksi', () => {
+  test('oikea määrä blogeja JSON muotoisena', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('kaikkien blogien hakeminen', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(testiBlogi.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
