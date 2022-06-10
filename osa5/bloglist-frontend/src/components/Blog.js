7da264ebbf5e98a88dc923de7a2b9ab1e988964a
zeenabtaher <zeenab.taher@gmail.com>
2022-06-10 21:44:11 +0300

    5.7 - step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,18 @@
+import TogglableText from "./TogglableText"
 const Blog = ({blog}) => (
   <div>
-    {blog.title} {blog.author}
+   <p>{blog.title} {blog.author}</p> 
+    <TogglableText buttonLabel="Katso">
+      <small>
+      Aihe: <i> {blog.title} </i>
+      <br />
+      Kirjoittaja: <i> {blog.author} </i>
+      <br />
+      Tykkäykset: <i> {blog.likes} </i> <button type = "submit">Tykkää</button>
+      <br />
+      Osoite: <i> {blog.url} </i>
+      </small>
+    </TogglableText>
   </div>  
 )
 
---
+++ osa5/bloglist-frontend/src/components/TogglableText.js
@@ -0,0 +1,33 @@
+import { useState, useImperativeHandle, forwardRef } from 'react'
+
+const TogglableText = forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    }
+  })
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <br />
+        <button onClick={toggleVisibility}>Piilota</button>
+      </div>
+    </div>
+  )
+})
+
+export default TogglableText 
